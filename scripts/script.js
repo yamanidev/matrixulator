39a8b6ce3f286310eb0dd73fb349f4e9fabd6e18
yamanidev <elamine.yamani@gmail.com>
2021-12-25 22:49:17 +0100

    Setting dimension of matrix functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,32 @@
 const dimensionSelector = document.getElementById("dimensionSelector");
+const matrix = document.getElementById("matrix");
 const calculateDetBtn = document.getElementById("calculateDetBtn");
 
 function getSelectedOption() {
     return dimensionSelector.options[dimensionSelector.selectedIndex].text;
 }
 
+function resetMatrix() {
+    while (matrix.lastChild) {
+        matrix.removeChild(matrix.lastChild);
+    }
+}
+
 // Reset selection
 dimensionSelector.selectedIndex = 0;
 
 dimensionSelector.addEventListener("change", () => {
+    const dimension = +getSelectedOption();
+    resetMatrix();
+
+    matrix.style.gridTemplateColumns = `repeat(${dimension}, 40px)`;
+
+    for (let i = 0; i < dimension; i++) {
+        for (let j = 0; j < dimension; j++) {
+            let inputEl = document.createElement("input");
+            matrix.appendChild(inputEl);
+        }
+    }
 
 });
 
